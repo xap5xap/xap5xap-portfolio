ea6fe2643b6825c3a915728f24bb35de7af92c67
xap5xap <xap5xap@gmail.com>
2016-06-22 16:02:50 -0500

    upload intro component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, AfterContentInit, AfterViewInit} from '@angular/core';
 import {NavbarComponent} from './navbar/navbar.component';
+import {IntroComponent} from './intro/intro.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [NavbarComponent]
+  directives: [NavbarComponent, IntroComponent]
 })
 export class AppComponent {
   title = "hola";
--- src/system-config.ts
+++ src/system-config.ts
@@ -34,6 +34,7 @@ const barrels: string[] = [
   'app',
   'app/shared',
   'app/navbar',
+  'app/intro',
   /** @cli-barrel */
 ];
 
