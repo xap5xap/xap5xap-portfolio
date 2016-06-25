2b94b356f92f1e63aa38d27885e2dc79acc6736b
xap5xap <xap5xap@gmail.com>
2016-06-25 10:53:52 -0500

    simple form validation errors

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, ControlGroup, Validators, Control, AbstractControl} from '@angular/common';
 
 @Component({
   moduleId: module.id,
@@ -7,13 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  contactForm: ControlGroup;
+  firstname: Control;
+  lastname: Control;
+  email: Control;
+  message: Control;
 
-  constructor() { }
+  constructor(formBuilder: FormBuilder) {
+    this.firstname = formBuilder.control('');
+    this.lastname = formBuilder.control('', Validators.compose([Validators.required]));
+    this.email = formBuilder.control('');
+    this.message = formBuilder.control('');
+
+    this.contactForm = formBuilder.group({
+      firstname: this.firstname,
+      lastname: this.lastname,
+      phone: formBuilder.control(''),
+      email: this.email,
+      message: this.message,
+    });
+  }
 
   ngOnInit() {
   }
 
-  send(form) {
-    console.log('form', form);
+  showErrors(): boolean {
+    for (let controlname in this.contactForm.controls) {
+      let control: AbstractControl = this.contactForm.find(controlname);
+      if (control.dirty && control.errors) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  send() {
+    console.log('form', this.contactForm.value);
+
   }
 }
